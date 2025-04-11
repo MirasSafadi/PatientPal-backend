f411e29157c85fd29b45472f5767ec321ba26787
KamelDokh <Kameldokhan2@gmail.com>
2025-04-11 11:43:02 +0300

    add login route

--- routes.py
+++ routes.py
@@ -67,6 +67,7 @@ def register():
         'message': 'A confirmation email has been sent. Please confirm your registration.',
         'code': 200,
     }), 200
+
 @flask_app.route('/confirm/<token>')
 def confirm_registration(token):
     data = utils.confirm_token(token)
@@ -95,3 +96,30 @@ def confirm_registration(token):
     # Create new user
     inserted_id = db_instance.add_document("users", user)
     return jsonify({ "msg": "User registered", "id": str(inserted_id) }), 201
+
+@flask_app.route('/login', methods=['POST'])
+def login():
+    logger.debug("Login endpoint (/login) called")
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username is None or password is None :
+            return jsonify({"error": "bad request"}),400
+        
+
+        logger.debug(f"Login attempt with username: {username}")
+
+        # חיפוש המשתמש במסד הנתונים
+        user = db_instance.get_document("users", {"username": username})
+
+        if user is not None:
+            if bcrypt.check_password_hash(pw_hash=user.get("password"), password=password):
+                logger.info(f"User '{username}' logged in successfully.")
+                token = generate_token({"username": username, "password": user.get("password")})
+                # hash the token and save in db
+                return jsonify({"message": "Login successful", "token": token}), 200
+        logger.warning(f"User '{username}' login attempt failed.")
+        return jsonify({"message": "login attempt failed."}), 404
+    return jsonify({"error": "Wrong method"}), 405
+
