f63a89e57b78f250251984529016cda48f7c9a89
MirasSafadi <safadimiras@gmail.com>
2025-04-19 18:14:26 +0300

    ..

--- routes.py
+++ routes.py
@@ -3,6 +3,7 @@ from crypto import auth_required, generate_token
 from mongodb_interface import MongoDBInterface
 from logger import Logger
 from app import flask_app, bcrypt
+import utils
 
 # Initialize database connection
 db_instance = MongoDBInterface()
@@ -21,18 +22,6 @@ def ping():
     logger.debug("Ping endpoint (/ping) called")
     return '<h1>Pong</h1>'
 
-@flask_app.route('/login', methods=['POST'])
-def login():
-    logger.debug("Login endpoint (/login) called")
-
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        if username is None or password is None:
-            return jsonify({"error": "bad request"}), 400
-
-        logger.debug(f"Login attempt with username: {username}")
-
 @flask_app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
@@ -40,26 +29,6 @@ def register():
     email = data.get('email', '').strip().lower()
     password = data.get('password')
     confirm_password = data.get('confirm_password')
-
-        # חיפוש המשתמש במסד הנתונים
-        user = db_instance.get_document("users", {"username": username})
-
-        if user is not None:
-            if bcrypt.check_password_hash(pw_hash=user.get("password"), password=password):
-                logger.info(f"User '{username}' logged in successfully.")
-                token = generate_token({"username": username, "password": user.get("password")})
-                
-                # 🔒 hash the token and save in db
-                hashed_token = bcrypt.generate_password_hash(token).decode('utf-8')
-                db_instance.update_document("users", {"username": username}, {"session_token": hashed_token})
-
-                return jsonify({"message": "Login successful", "token": token}), 200
-
-        logger.warning(f"User '{username}' login attempt failed.")
-        return jsonify({"message": "login attempt failed."}), 404
-    return jsonify({"error": "Wrong method"}), 405
-
-
     #inserted_id = db_interface.add_document("users", user)
 
     #return jsonify({ "msg": "User registered", "id": str(inserted_id) }), 201
@@ -115,7 +84,7 @@ def login():
         if user is not None:
             if bcrypt.check_password_hash(pw_hash=user.get("password"), password=password):
                 logger.info(f"User '{username}' logged in successfully.")
-                token = generate_token({"username": username, "password": user.get("password")})
+                token = generate_token({"username": username, "hashed_password": user.get("password")})
                 
                 # 🔒 hash the token and save in db
                 hashed_token = bcrypt.generate_password_hash(token).decode('utf-8')
