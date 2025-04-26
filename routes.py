d3670922a1182c7c086a09f5f571f8cfc006a2be
MirasSafadi <45172450+MirasSafadi@users.noreply.github.com>
2025-04-26 13:10:25 +0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -29,6 +29,56 @@ def register():
     email = data.get('email', '').strip().lower()
     password = data.get('password')
     confirm_password = data.get('confirm_password')
+    id_number=data.get('id_number')
+
+    # Optional profile fields
+    first_name = data.get('first_name', '').strip()
+    last_name = data.get('last_name', '').strip()
+    phone = data.get('phone', '').strip()
+    birthdate = data.get('birthdate', '').strip()
+    gender = data.get('gender', '').strip()
+    address = data.get('address', '').strip()    
+    # Validate required
+    utils.validation(username, email, password, confirm_password)
+  
+    filter_criteria_username =  {"username": username }
+    filter_criteria_email =  {"email": email}
+    if db_instance.get_document("users", filter_criteria_username) or db_instance.get_document("users", filter_criteria_email) : #checking if the user exist
+        return jsonify({ "msg": "User already exists" }), 400
+
+    # Hash password and store
+    hashed_pw = bcrypt.generate_password_hash(password).decode('utf-8')
+    user = {
+        "username": username,
+        "email": email,
+        "password": hashed_pw,
+        "profile": {
+            "first_name": first_name,
+            "last_name": last_name,
+            "phone": phone,
+            "birthdate": birthdate,
+            "gender": gender,
+            "address": address
+        }
+    }
+
+
+    token = utils.generate_registration_token(user)
+        
+# Create the email
+    msg = EmailMessage()
+    msg['Subject'] = 'Confirm your account'
+    msg['From'] = settings.MAIL_USERNAME
+    msg['To'] = user['email']
+    msg.set_content(f'Hi there!\n\nThank you for signing up. Please confirm your email by clicking the link below:\n\n{request.host_url}/confirm/{token}')
+# Send the ema
+    with smtplib.SMTP('smtp.gmail.com',587) as server:
+        server.starttls()
+        server.login(settings.MAIL_USERNAME,settings.MAIL_PASSWORD)
+        server.send_message(msg)
+
+
+
     #inserted_id = db_interface.add_document("users", user)
 
     #return jsonify({ "msg": "User registered", "id": str(inserted_id) }), 201
@@ -36,7 +86,6 @@ def register():
         'message': 'A confirmation email has been sent. Please confirm your registration.',
         'code': 200,
     }), 200
-
 @flask_app.route('/confirm/<token>')
 def confirm_registration(token):
     data = utils.confirm_token(token)
